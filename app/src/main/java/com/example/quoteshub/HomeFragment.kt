1b7aa7889fa7bf61af8ad2abf728c7de902204bf
alim1496 <alim1496@gmail.com>
2019-09-03 01:34:46 +0600

    fetch data from database

--- app/src/main/java/com/example/quoteshub/HomeFragment.kt
+++ app/src/main/java/com/example/quoteshub/HomeFragment.kt
@@ -2,10 +2,14 @@ package com.example.quoteshub
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -18,13 +22,30 @@ private const val ARG_PARAM2 = "param2"
  *
  */
 class HomeFragment : Fragment() {
+    var db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    var quotes: MutableList<Quote> = mutableListOf<Quote>()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        FireBase().getData("quotes")
-        // Inflate the layout for this fragment
+        val layoutManager = LinearLayoutManager(activity)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+
+        var adapter : QuotesAdapter? = null
+
+        db
+            .collection("quotes")
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    Log.d("success", "${document.id} => ${document.data}")
+                    quotes.add(Quote(document.get("quote").toString(), document.get("source").toString()))
+                }
+                recyclerView.layoutManager = layoutManager
+                adapter = QuotesAdapter(activity, quotes)
+                recyclerView.adapter = adapter
+            }
+            .addOnFailureListener { exception ->
+                Log.w("failure", "Error getting documents: ", exception)
+            }
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
---
+++ app/src/main/java/com/example/quoteshub/QuoteModel.kt
@@ -0,0 +1,3 @@
+package com.example.quoteshub
+
+data class Quote(var title:String, var source:String)
