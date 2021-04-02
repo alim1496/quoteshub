1b50e234a0d0a2ec48a9aa50d7d4004012519a1b
alim1496 <alim1496@gmail.com>
2021-04-02 16:45:28 +0600

    fix same youtube video in recyclerview

--- app/src/main/java/com/appwiz/quoteshub/fragments/VideoFragment.kt
+++ app/src/main/java/com/appwiz/quoteshub/fragments/VideoFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ProgressBar
 import android.widget.RelativeLayout
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.MutableLiveData
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.appwiz.quoteshub.R
